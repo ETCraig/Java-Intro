7771d0138f44a02eb6e88588a89aa9bed1f51545
Ethan Craig <Ethan.Craig@healthbi.com>
2020-08-08 16:36:50 -0700

    Removed Duplications.

--- src/com/etcraig/javaIntro/MultiplicationRunner.java
+++ src/com/etcraig/javaIntro/MultiplicationRunner.java
@@ -5,8 +5,8 @@ public class MultiplicationRunner {
 	public static void main(String[] args) {
 		MultiplicationTable table = new MultiplicationTable();
 		//table.print();
-		//table.print(5);
-		table.print(6, 11, 20);
+		table.print(5);
+		// table.print(6, 11, 20);
 	}
 	
 }
--- src/com/etcraig/javaIntro/MultiplicationTable.java
+++ src/com/etcraig/javaIntro/MultiplicationTable.java
@@ -2,15 +2,11 @@ package com.etcraig.javaIntro;
 
 public class MultiplicationTable {
 	void print() {
-		for(int i = 1; 1 <= 10; i++) {
-			System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
-		}
+		print(5, 1, 10);
 	}
 	
 	void print(int table) {
-		for(int i = 1; i <= 10; i++) {
-			System.out.printf("%d * %d = %d", table, i, table * i).println();
-		}
+		print(table, 1, 10);
 	}
 	
 	void print(int table, int from, int to) {
